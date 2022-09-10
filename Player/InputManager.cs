d4e5fcfd3c7c4864330febe6bfa1ec9728ccc62e
EzzatEsam <s-ezzat.eisawy@zewailcity.edu.eg>
2022-09-11 01:44:02 +0200

    added player

---
+++ Player/InputManager.cs
@@ -0,0 +1,28 @@
+using Godot;
+using System;
+
+public class InputManager : Node2D
+{
+    public bool MoveRight { get { return _right; } }
+    public bool MoveLeft { get { return _left; } }
+    public bool MoveUp { get { return _up; } }
+    public bool MoveDown { get { return _down; } }
+    private bool _right;
+    private bool _left;
+    private bool _up;
+    private bool _down;
+
+    public override void _Ready()
+    {
+
+    }
+
+
+    public override void _Process(float delta)
+    {
+        _down = Input.IsActionPressed("move_down");
+        _right = Input.IsActionPressed("move_right") ;
+        _left = Input.IsActionPressed("move_left");
+        _up = Input.IsActionPressed("move_up");
+    }
+}
